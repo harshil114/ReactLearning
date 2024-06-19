90b02c9e1aa037a88d1ce4a2942cfbf57bd5c054
harshil114 <harshilvispute114@gmail.com>
2024-06-19 18:05:40 +0530

    Add parcel to project

--- Lecture2/App.js
+++ Lecture2/App.js
@@ -1,11 +1,5 @@
-// const heading = React.createElement(
-//     "h1",
-//     {id:"heading"},
-//     "Hello"
-// );
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:"parent"},[
 
