14319986d40ca31569bfceb6502d75799ff07fc6
harshil114 <harshilvispute114@gmail.com>
2024-06-25 14:00:26 +0530

    add components and component composition

--- Lecture3/App.js
+++ Lecture3/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1>Heading from JSX</h1>
+// const jsxHeading = <h1>Heading from JSX</h1>
+
+const TitleComponent = () => (
+    <h1>Learn React</h1>
+);
+
+const HeadingComponent = () => (
+    <div>
+        <TitleComponent />
+        <h3>Hello React</h3>
+    </div>
+
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading)
+root.render(<HeadingComponent />)
