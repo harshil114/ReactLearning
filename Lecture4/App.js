d473bdea14c88bd81ce3d21a8dcba63e8d284aeb
harshil114 <harshilvispute114@gmail.com>
2024-06-25 17:56:26 +0530

    Add Header, Body, and RestaurantCard components for food delivery app

--- Lecture4/App.js
+++ Lecture4/App.js
@@ -1,21 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const jsxHeading = <h1>Heading from JSX</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        />
+      </div>
 
-const TitleComponent = () => (
-    <h1>Learn React</h1>
-);
-const number = 100;
-const HeadingComponent = () => (
-    <div>
-        <h4>{number + number}</h4>
-        <h4>{10 + 20}</h4>
-        <TitleComponent />
-        <h3>Hello React</h3>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
+  );
+};
 
-)
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7"
+        alt="res-logo"
+      />
+      <h3>Pizza Hut</h3>
+      <h4>Pizza, Italian</h4>
+      <h4>4.4 stars</h4>
+      <h4>30 mins</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />)
+root.render(<AppLayout />);
