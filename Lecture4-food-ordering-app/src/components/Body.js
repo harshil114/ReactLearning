593ae6b3f4cb3b70ada9bfbccdc347ffd0723a2c
harshil114 <harshilvispute114@gmail.com>
2024-06-27 20:08:34 +0530

    add feature for top rated restaurant using useState hook

--- Lecture4-food-ordering-app/src/components/Body.js
+++ Lecture4-food-ordering-app/src/components/Body.js
@@ -1,12 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-  
+import { useState } from "react";
+
 const Body = () => {
+  const [topRatedRestaurant,settopRatedRestaurant] = useState(resList);
+  const handleFilter = () => {
+    const filterList = resList.filter((res) => res.info.avgRating > 4.5 );
+    settopRatedRestaurant(filterList);
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={handleFilter}>
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {topRatedRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
