dfdd7dd8686ebf73b6dd6762c86693c3202a2cf2
harshil114 <harshilvispute114@gmail.com>
2024-07-05 19:21:14 +0530

    fix search functionality

--- Lecture4-food-ordering-app/src/components/Body.js
+++ Lecture4-food-ordering-app/src/components/Body.js
@@ -1,34 +1,16 @@
 import RestaurantCard from "./RestaurantCard";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import resList from "../utils/mockData";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurant, setListOfRestaurant] = useState(resList);
   const [searchText, setSearchText] = useState("");
-  const [filteredRes , setFilteredRes] = useState(resList);
-  // const [error, setError] = useState(false);
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // const fetchData = async () => {
-  //   try {
-  //     const data = await fetch(
-  //       "https://www.swiggy.com/mapi/homepage/getCards?lat=23.022505&lng=72.5713621"
-  //     );
-  //     const json = await data.json();
-  //     setListOfRestaurant(json?.data?.success?.cards);
-  //   } catch (e) {
-  //     setError(true);
-  //   }
-  // };
-
+  const [filteredRestaurant , setFilteredRestaurant] = useState(resList);
+ 
   const handleFilter = () => {
-    const filterResList = resList.filter((res) => res.info.avgRating > 4.5);
-     setListOfRestaurant(filterResList);
-     setFilteredRes(filterResList);
+    const filteredList = resList.filter((res) => res.info.avgRating > 4.5);
+     setFilteredRestaurant(filteredList);
   };
 
   return listOfRestaurant.length === 0 ? (
@@ -50,7 +32,7 @@ const Body = () => {
               const filteredRestaurant = listOfRestaurant.filter((res) =>
                 res.info.name.toLowerCase().includes(searchText.toLowerCase())
               );
-              setFilteredRes(filteredRestaurant);
+              setFilteredRestaurant(filteredRestaurant);
             }}
           >
             Search
@@ -60,19 +42,9 @@ const Body = () => {
           Top Rated Restaurants
         </button>
       </div>
-      {/* <div className="res-container">
-        {error ? (
-          <p style={{ color: "red", fontSize: "1.8rem" }}>
-            Server error. Please try again later.
-          </p>
-        ) : (
-          listOfRestaurant.map((restaurant) => (
-            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
-          ))
-        )}
-      </div> */}
+
       <div className="res-container">
-        {filteredRes.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
