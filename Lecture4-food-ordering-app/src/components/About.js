e032dfc931024ff1fe328203dcaf537c70d306ae
harshil114 <harshilvispute114@gmail.com>
2024-07-08 19:06:30 +0530

    add class based component

--- Lecture4-food-ordering-app/src/components/About.js
+++ Lecture4-food-ordering-app/src/components/About.js
@@ -1,10 +1,18 @@
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>A swiggy App</h2>
-        </div>
-    )
+import User from "./User";
+import UserClass from "./UserClass";
 
-}
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>A swiggy App</h2>
+      <User name={"Harshil Vispute (function)"} />
+      <UserClass
+        name={"Harshil Vispute (Class)"}
+        location={"Ahmedabad (class)"}
+        twitter={"@harshilvispute (class)"}
+      />
+    </div>
+  );
+};
 export default About;
---
+++ Lecture4-food-ordering-app/src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name}) => {
+  return (
+    <div className="user-card">
+      <h2>Name : {name}</h2>
+      <h3>Location : Ahmedabad</h3>
+      <h4>Contact : @harshilvispute</h4>
+    </div>
+  );
+};
+export default User;
