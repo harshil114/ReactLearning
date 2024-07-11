3f4f2f34231a219f15ffd3b37eff4a9802a0d4e4
harshil114 <harshilvispute114@gmail.com>
2024-07-11 16:02:32 +0530

    learn state variable in class component

--- Lecture4-food-ordering-app/src/components/UserClass.js
+++ Lecture4-food-ordering-app/src/components/UserClass.js
@@ -2,12 +2,27 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
-    const {name,location,twitter} = this.props;
+    const { name, location, twitter } = this.props;
     return (
       <div className="user-card">
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Count
+        </button>
+        <h1>{this.state.count}</h1>
+        <h1>{this.state.count2}</h1>
         <h2>Name : {name}</h2>
         <h3>Location : {location}</h3>
         <h4>Contact : {twitter}</h4>
