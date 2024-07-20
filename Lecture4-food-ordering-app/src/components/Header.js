dce286d4fcc5f50802d12178cbe88ddd1ae1a81a
harshil114 <harshilvispute114@gmail.com>
2024-07-20 19:24:28 +0530

    style header

--- Lecture4-food-ordering-app/src/components/Header.js
+++ Lecture4-food-ordering-app/src/components/Header.js
@@ -7,26 +7,26 @@ const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
   const onlineStatus = useOnlineStatus();
   return (
-    <div className="header">
+    <div className="flex justify-between items-center">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} />
+        <img className="w-28" src={LOGO_URL} />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status: {onlineStatus ? "✅" : "🔴"}</li>
-          <li>
+      <div className="nav-items mr-9">
+        <ul className="flex">
+          <li className="mr-7 text-lg font-medium">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="mr-7 text-lg font-medium">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="mr-7 text-lg font-medium">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="mr-7 text-lg font-medium">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="mr-7 text-lg font-medium">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="mr-7 text-lg font-medium">Cart</li>
           <button
             className="login"
             onClick={() => {
